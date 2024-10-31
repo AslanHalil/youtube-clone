2d44305294863e343d4bbd23d29f6cc064e923bf
AslanHalil <AslanCHalil@outlook.com>
2024-10-31 15:24:06 +0200

    Implemented a simple feature that takes a video and sclales the resolution to 360p. Everything is done locally and using a POST request through Postman.

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-});
+app.post("/process-video", (req, res) => {
+    //Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad request: Missing File Path.");
+    }
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Processing finished succesfully.");
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal server error: ${err.message}`);
+        })
+        .save(outputFilePath)
+})
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`video processing service listening at http://localhost:${port}`);
